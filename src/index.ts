c20661f6116ba0e1c4403498cbbb62d0aeaf06f9
gedeapriana <gedeapriana36@gmail.com>
2024-12-07 18:27:31 +0800

    ts + express boilerplate

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: "hello from express + typescript" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
